dfc0c55069ac931829a0ecca0953438d98f46fe7
RoxNcode <jose.holanda@ee.ufcg.edu.br>
2022-01-03 10:35:25 -0300

    Classes

--- src/Aula09-Classes/Index.ts
+++ src/Aula09-Classes/Index.ts
@@ -1,11 +1,30 @@
 export class Empresa {
     public readonly nome: string;
+    
+    private readonly colaboradores: Colaborador[] = [];
 
-    constructor(nome: string) {
+    protected readonly cnpj: string;
+
+    constructor(nome: string, cnpj: string) {
         this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    adicionaColaborador(colaborador: Colaborador) {
+        this.colaboradores.push(colaborador);
     }
 }
 
-const empresa1 = new Empresa('Company');
+export class Colaborador {
+    constructor(
+        public readonly nome: string,
+        public readonly sobrenome: string,
+    ) {}
+}
 
-console.log(empresa1.nome);
+const empresa1 = new Empresa('Company','11.111.111/0001-11');
+const colaborador1 = new Colaborador('Luiz', 'Otavio');
+
+empresa1.adicionaColaborador(colaborador1);
+
+console.log(empresa1);
